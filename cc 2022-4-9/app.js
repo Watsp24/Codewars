e7303d413ce7350579792a0f9f4056b8ff9f2d94
Watson Pierre <watsonpierrejr@gmail.com>
2022-04-30 13:03:50 -0400

    Is Prime

---
+++ cc 2022-4-9/app.js
@@ -0,0 +1,16 @@
+//Define a function that takes one integer argument and returns logical 
+//value true or false depending on if the integer is a prime.
+
+//Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 
+//that has no positive divisors other than 1 and itself.
+
+function isPrime(num) {
+    //TODO
+    // let num;
+    if( i = 1, i % num == 0, i++ ){
+        console.log(`${num} is a prime number!`)
+    }else if( i = 1, i % num !== 0, i++){
+        console.log(`${num} is not a prime numbeer!`)
+    }
+  }
+  isPrime(10)
