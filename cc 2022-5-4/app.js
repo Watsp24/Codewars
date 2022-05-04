4c629d6d4daea6319dcb315878b092af1dfba476
Watson Pierre <watsonpierrejr@gmail.com>
2022-05-04 13:22:51 -0400

    Code Wars in today: Remove String Space

--- cc 2022-5-4/app.js
+++ cc 2022-5-4/app.js
@@ -1,7 +1,6 @@
 //Simple, remove the spaces from the string, then return the resultant string.
 
 function noSpace(x){
- 
-     new = ' 8 j 8   mBliB8g  imjB8B8  jl  B  '
- console.log(new.split().join())
-}
+    return x.split(' ').join('') //using split(),remember to keep the extra space for the delimiter or wont work
+  }
+  
