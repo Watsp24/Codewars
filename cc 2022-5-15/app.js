e72cc81cecfdff5736a7e1e1d7d81650b479bb4c
Watson Pierre <watsonpierrejr@gmail.com>
2022-05-15 23:36:11 -0400

    track changes

--- cc 2022-5-15/app.js
+++ cc 2022-5-15/app.js
@@ -9,9 +9,9 @@
 //The number can be negative already, in which case no change is required.
 //Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
-function makeNegative(num) {
+function makeNegative( num ) {
     // Code
-    return -Math.abs(num);
+    return -Math.abs( num );
   }
 makeNegative(15)
 
