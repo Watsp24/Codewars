297dce7185c210f331233125053347c301ca0db8
Watson Pierre <watsonpierrejr@gmail.com>
2022-05-29 12:38:39 -0400

    switch

--- cc 2022-5-29/app.js
+++ cc 2022-5-29/app.js
@@ -6,7 +6,7 @@
 // 123 --> "123"
 // 999 --> "999"
 
-function numberToString( num ) {
+function numberToString(numb) {
     // Return a string of the number here!
-    return num.toString()
+    return numb.toString()
   }
