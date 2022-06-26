5a4d51d250e353cdfb5f5aa1c951bf72c0c2734c
Watson Pierre <watsonpierrejr@gmail.com>
2022-06-25 20:59:49 -0400

    Make Uppercase

---
+++ cc 2022-6-24/app.js
@@ -0,0 +1,12 @@
+// Write a function which converts the input string to uppercase.
+
+//Prep  // Parameters Return Example Psuedo Code
+//parameters - string 
+//return console log the string in UpperCase
+//example
+
+function makeUpperCase(str) {
+    // Code here
+    //take the string and capitalize
+    return str.toUpperCase();
+  }
