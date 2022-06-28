b4bff18da925d9eaf3829dd631869d7060143222
Watson Pierre <watsonpierrejr@gmail.com>
2022-06-27 22:38:04 -0400

    #1 School Paperwork

---
+++ cc 2022-6-26/app.js
@@ -0,0 +1,28 @@
+DESCRIPTION:
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+
+
+//Prep  // Parameters Return Example Psuedo Code
+//Parameters n,m integers
+//return number of pages needed, return 0 if n,m < 0
+//example
+
+function paperwork(n, m) {
+    //get the value of n, m 
+    //if n < 0, 
+    if(n < 0 || m < 0) {
+        return 0;
+    }else{
+       (n > 0 || m > 0)
+         return n * m;
+       
+    }
+  
+}
