12addfc4cf0cf730ab82e47dbe573141e8cd9748
Watson Pierre <watsonpierrejr@gmail.com>
2022-07-07 13:54:55 -0400

    DNA to RNA

---
+++ cc 2022-7-7/app.js
@@ -0,0 +1,21 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+//Prep Parameters Return Example Psuedo
+//Parameters is DNA andd RNA
+//Given DNA string to RNA string
+//Example GCAT TO GCAU
+
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+
+    return dna.split("T").join("U");
+  }
