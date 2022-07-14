3f07bc0c587cecb9c6681aa74c2698ba14d261af
Watson Pierre <watsonpierrejr@gmail.com>
2022-07-14 12:49:26 -0400

    Convert String to Array

---
+++ cc 2022-7-14/app.js
@@ -0,0 +1,20 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+
+//Prep Parameters Return Example Psuedo
+// Parameter is a string
+// Return string into an array of words
+// Example "Mary Loves" --> ["Mary", "Loves"]
+
+function stringToArray(string){
+
+	// code code code
+   return string.split(' ');
+
+}
