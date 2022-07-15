3a44bd595c5cef6bd979e0669136ddaf90c294e4
Watson Pierre <watsonpierrejr@gmail.com>
2022-07-15 12:59:11 -0400

    Function 3: Multiplying 2 numbers

---
+++ cc 2022-7-15/app.js
@@ -0,0 +1,18 @@
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+
+//Prep Parameters Return Example Psuedo
+//parameters - two numbers
+//return - answer after multiplied
+//Example - provide 
+
+function multiply(a,b){
+let result = (a*b)
+return result 
+} 
+
+// Write here your multiply-function
