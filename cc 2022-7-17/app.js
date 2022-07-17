dc748cfc0d1e679cc3823e7020e517e3ed603021
Watson Pierre <watsonpierrejr@gmail.com>
2022-07-17 16:54:10 -0400

    If you cant sleep, just count sheep!

---
+++ cc 2022-7-17/app.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+//Prep Parameters Return Example Psuedo
+
+var countSheep = function (num){
+    //your code here
+    //num += 1
+    if(i = 1, i < num, i++){
+      
+     return(`${num} sheep... ${num} sheep... ${num} sheep...`) 
+    }
+    
+  }
+  
