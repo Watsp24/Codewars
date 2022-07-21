88a13ace42d749e4eb16475cff83d584b9f77be6
Watson Pierre <watsonpierrejr@gmail.com>
2022-07-21 08:24:01 -0400

    GrassHopper Personalized Message

---
+++ cc 2022-7-21/app.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+//Prep Parameters Return Example Psuedo
+//parameters name, owner
+//return Hello boss when name equals owner and hello guest when it doesnt
+//example
+
+
+function greet (name, owner) {
+    // Add code here
+  }
