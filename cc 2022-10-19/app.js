510b9fef0e6351ec2443f6e5e16de0f002033a0b
Watson Pierre <watsonpierrejr@gmail.com>
2022-08-29 12:29:20 -0400

    Implement Array.prototype.filter"

---
+++ cc 2022-10-19/app.js
@@ -0,0 +1,15 @@
+// Write a function thatt when given a URL as a String, parses out just the domain name and returns it as a string. For Example: 
+// domainName('http://github.com/carbonfive/raygun') == 'github'
+// domainName('http://www.zombie-bites.com') == 'zombie-bites'
+// domainName('http://www.cnet.com') == 'cnet'
+
+
+//Prep Parameters Return Example Psuedo
+//Parameter is a URL string
+//return just the domain name as a string
+//example above
+
+
+function parseDomain(){
+    
+}
--- cc 2022-8-29/app.js
+++ cc 2022-8-29/app.js
@@ -13,5 +13,15 @@
 Array.prototype.filter = function (func) {
     //nothing here, you will feel pretty awesome to write down by yourself
     //have fun :)
+
+    var result = [];
+    for(let i =  0; i < this.length; i++){
+      if(func(this[i])){
+        result.push(this[i]);
+      }
+    }
+    return result;
   }
+
+  
   
