0b6ba55aa772a217e07e0dcbbb96e2d30fb15d57
Watson Pierre <watsonpierrejr@gmail.com>
2022-08-30 19:29:01 -0400

    Find Mean

---
+++ cc 2022-8-29/app.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// What we want to implement is Array.prototype.filter() function, just like the existing Array.prototype.filter(). Another similar function is _.filter() in underscore.js and lodash.js.
+
+// The usage will be quite simple, like:
+
+// [1, 2, 3, 4, 5].filter(num => num > 3) == [4, 5]
+
+
+//Prep Parameters Return Example Psuedo
+
+
+
+Array.prototype.filter = function (func) {
+    //nothing here, you will feel pretty awesome to write down by yourself
+    //have fun :)
+  }
+  
