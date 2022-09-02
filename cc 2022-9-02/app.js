695ec8e223ba720e9707f1c6e00da01b3cbe43bf
Watson Pierre <watsonpierrejr@gmail.com>
2022-09-02 09:37:00 -0400

    Sort Arrrays -1

---
+++ cc 2022-9-02/app.js
@@ -0,0 +1,15 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+//Prep Parameters Return Example Psuedo
+//parameter array of inputs
+//return a sorted list in lexicographical order
+//example 
+
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort();
+}
+
+
