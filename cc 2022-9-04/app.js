d9ff6b1d82cbb6a564ddf850548d061434b5908b
Watson Pierre <watsonpierrejr@gmail.com>
2022-09-04 10:33:19 -0400

    Number in String

---
+++ cc 2022-9-04/app.js
@@ -0,0 +1,16 @@
+// In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+// For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+// Good luck!
+
+//Prep Parameters Return Example Psuedo
+//parameter string with letters and numbers
+//return largest number in the group
+//example "gh12cdy695m1") = 695
+
+
+function solve(s){
+    //..pseudo
+    return Math.max(...s.match(/\d+/g));
+    };
