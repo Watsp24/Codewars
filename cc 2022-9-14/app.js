9ea8be83ece17449737cb612c8859abbcab7b864
Watson Pierre <watsonpierrejr@gmail.com>
2022-09-14 13:22:01 -0400

    Working with Arrays/why your code fails

---
+++ cc 2022-9-14/app.js
@@ -0,0 +1,19 @@
+// In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+// Good luck!
+
+// Hint: watch out for side effects.
+
+//Prep Parameters Return Example Psuedo
+//Parameters - an array of elements
+//return - numbers without last element
+//example 
+
+
+function withoutLast(arr) {
+    // Fix it
+    // removes the last element
+    return arr.slice(0,-1);
+   
+  }
+
