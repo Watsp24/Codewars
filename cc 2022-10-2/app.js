67e8a13320486c308fc28002949a24172a5fbb66
Watson Pierre <watsonpierrejr@gmail.com>
2022-10-02 11:08:04 -0400

    Adding an Item to  Array

--- cc 2022-10-2/app.js
+++ cc 2022-10-2/app.js
@@ -1,37 +1,20 @@
-// We want an array, but not just any old array, an array with contents!
 
-// Write a function that produces an array with the numbers 0 to N-1 in it.
+// DESCRIPTION:
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
 
-// For example, the following code will result in an array containing the numbers 0 to 4:
+// The websites array has already been defined for you using the following code:
 
-// arr(5) // => [0,1,2,3,4]
-// Note: The parameter is optional. So you have to give it a default value.
+
+// add the value "codewars" to the already defined websites array
+
 
 
 //Prep Parameters Return Example Psuedo
-//parameters default value
-//return an array of numbers 
-//example arr(5) // =>  [0,1,2,3,4]
+//parameters - empty array
+//return - array filled with string
+//example ['codewars']
 
+var websites = [];
 
-//psuedo n must = n.length
-const arr = N => [ /* the numbers 0 to N-1 */ ];
-
-function arr(n){
-    let result = [ ]
-
-    for( let i = 0; i < n; i++){
-        result.push[i];
-}
-return result;
-
-}
-
-//const arr = N => [ /* the numbers 0 to N-1 */ ];
-function arr(N){
-    let arr = [];
-    for (let i = 0; i < N; i++){
-      arr.push(i)
-    }
-    return arr;
-    }
+websites.push('codewars');
