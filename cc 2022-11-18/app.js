8f33eb5c5c52ccc7eaddf557baed19966b736846
Watson Pierre <watsonpierrejr@gmail.com>
2022-11-18 10:26:20 -0500

    Alan Partridge II

---
+++ cc 2022-11-18/app.js
@@ -0,0 +1,24 @@
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Note: Input will either be a positive integer (or a string for untyped languages).
+
+//prep
+//parameter is a positive integer or a string
+//return its hotter than the sun or help yourself to a honeycomb yorkie for the glovebox.
+
+function apple(x){
+
+    let num = x*x;
+
+//    if( num >= 1000 ) {
+//     return "Its hotter than the sun!!";
+//    }else{
+//     return 
+//     "help yourself to a honeycomb yorkie for the glovebox";
+//    }
+return num >= 1000 ? "It's hotter than the sun!!" : 'Help yourself to a honeycomb Yorkie for the glovebox.';
+ 
+}
+
+ 
