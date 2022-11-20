2bff5177647d54726d34b5521bc1b09b711ea86f
Watson Pierre <watsonpierrejr@gmail.com>
2022-11-20 11:13:51 -0500

    regular ball super ball

---
+++ cc 2022-11-20/app.js
@@ -0,0 +1,28 @@
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+
+//Prep Parameters Return Example Psuedo
+
+
+var Ball = function(ballType) {
+    // your code goes here
+    this.ballType = ballType ? ballType : "regular";
+
+    // var Ball = function(ballType) {
+    //     if (!ballType){
+    //       this.ballType = 'regular';
+    //     }
+    //     else {
+    //       this.ballType= 'super';
+    //     }
+    //   };
+  };
+
