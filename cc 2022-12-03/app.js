096166a653c9e22e3c8e9cf643e3166490f4daef
Watson Pierre <watsonpierrejr@gmail.com>
2022-12-03 15:56:20 -0500

    Keep up the hoop

--- cc 2022-12-03/app.js
+++ cc 2022-12-03/app.js
@@ -10,7 +10,7 @@
 
 function hoopCount (n) {
     //your code goes here   
-   if(n > 10){
+   if(n >= 10){
      return "Great, now move on to tricks"
    }else
      return "Keep at it until you get it"
