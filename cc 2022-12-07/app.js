7c18b26883f50a48c00318e8270ed4fe2769deb6
Watson Pierre <watsonpierrejr@gmail.com>
2022-12-07 22:31:19 -0500

    localize the barycenter of a string

---
+++ cc 2022-12-07/app.js
@@ -0,0 +1,53 @@
+// The medians of a triangle are the segments that unit the vertices with the midpoint of their opposite sides. The three medians of a triangle intersect at the same point, called the barycenter or the centroid. Given a triangle, defined by the cartesian coordinates of its vertices we need to localize its barycenter or centroid.
+
+// The function bar_triang() or barTriang or bar-triang, receives the coordinates of the three vertices A, B and C  as three different arguments and outputs the coordinates of the barycenter O in an array [xO, yO]
+
+// This is how our asked function should work: the result of the coordinates should be expressed up to four decimals, (rounded result).
+
+// You know that the coordinates of the barycenter are given by the following formulas.
+
+// source: imgur.com
+
+// For additional information about this important point of a triangle see at: (https://en.wikipedia.org/wiki/Centroid)
+
+// Let's see some cases:
+
+// barTriang([4, 6], [12, 4], [10, 10]) ------> [8.6667, 6.6667]
+
+//Prep Parameters Return Example Psuedo
+// Parameters -the coordinates of 3 vertices
+// returns cooridinates as an array [x0, y0]
+// example -  barTriang([4, 6], [12, 4], [10, 10]) ------> [8.6667, 6.6667]
+// psuedo
+
+
+function barTriang(p1, p2, p3){
+    //your code here
+   let x = (p1[0] + p2[0] + p3[0]) / 3;
+   let y = (p1[1] + p2[1] + p3[1]) / 3;
+
+   let result = [parseFloat(x.toFixed(4)), parseFloat(y.toFixed(4))];
+
+   
+    return result;
+
+  }
+// barTriang([4, 2], [12, 2], [6, 10]) ------> [7.3333, 4.6667]
+// The given points form a real or a degenerate triangle but in each case the above formulas can be used.
+
+// Enjoy it and happy coding!!
+
+
+
+// function barTriang(p1, p2, p3){
+//     //your code here
+//    let avg1 = (p1[x] + p2[x] + p3[x]) / 3;
+//    let avg2 = (p1[y] + p2[y] + p3[y]) / 3;
+
+//    let positionAvg1 = parseFloat(x.toFixed(4));
+//    let positionAvg2 = parseFloat(y.toFixed(4));
+
+   
+//     return [positionAvg1, positionAvg2];
+
+//   }
