bd5659f4c36e75c8b8531f3f50d9930c712a8568
Watson Pierre <watsonpierrejr@gmail.com>
2023-01-07 15:49:33 -0500

    return the day

---
+++ cc 2023-2-10/cc 2023-1-7/app.js
@@ -0,0 +1,42 @@
+// DESCRIPTION:
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+////Prep Parameters Return Example Psuedo
+
+
+
+//Parameter - takes in a number / integer 1-7
+//return - corresponding day of the week
+//example 3 returns tuesday
+
+function whatday(num) { 
+
+    // put your code here
+    if(num == 1){
+        return "Sunday"
+    }else if(num == 2){
+        return "Monday"
+    }else if(num == 3){
+        return "Tuesday"
+    }else if(num == 4){
+        return "Wednesday"
+    }else if(num == 5){
+        return "Thursday"
+    }else if(num == 6){
+        return "Friday"
+    }else if(num == 7){
+        return "Saturday"
+    }else {
+        return "Wrong, please enter a number between 1 and 7" 
+  }
+}
+
