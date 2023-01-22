53b57a470f961417efc3d443775658594c505237
Watson Pierre <watsonpierrejr@gmail.com>
2023-01-22 14:14:36 -0500

    No Loops 2

---
+++ cc 2023-1-22/app.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// *** No Loops Allowed ***
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+
+////Prep Parameters Return Example Psuedo
+
+
+function check(a,x){
+    return a.includes(x);
+};
