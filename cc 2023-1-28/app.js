b9d418f06f2b982fc9229ebe5ad4c122f40f0433
Watson Pierre <watsonpierrejr@gmail.com>
2023-01-28 14:31:34 -0500

    number of carousel turns

---
+++ cc 2023-1-28/app.js
@@ -0,0 +1,17 @@
+// Carousel
+// Write a program that launches a carousel for 10 turns, showing the turn number each time.
+
+// When it's done, improve it so that the number of turns is given by the user.
+
+
+
+////Prep Parameters Return Example Psuedo
+// parameter - integer of 10
+// returns - the number each time
+// 
+
+const num = Number(prompt("Enter the number of carousel turns"));
+
+for(i = 1; i <=num; i++){
+    console.log(i)
+}
