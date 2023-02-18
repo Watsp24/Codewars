a30dc95763973ea0bd2212d38e363aa852807ddc
Watson Pierre <watsonpierrejr@gmail.com>
2023-02-18 15:26:41 -0500

    multiplication off number input/improved

---
+++ cc 2023-2-17/cc 2023-2-18/app.js
@@ -0,0 +1,14 @@
+// Multiplication table
+// Write a program that asks the user for a number, then shows the multiplication table for this number.
+
+
+const numb = prompt('Enter a number for multiplication table')
+
+if(numb < 2 || numb > 9){
+    console.log('insufficient value')
+}else if(numb > 2 && numb <= 9){
+
+    for(i=1;i<=numb;i++){
+        console.log(numb * i)
+    }
+}
