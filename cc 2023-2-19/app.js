36b1ff8f3aded86e4d4f70a8698454925e4710e2
Watson Pierre <watsonpierrejr@gmail.com>
2023-02-19 18:42:10 -0500

    range counting

---
+++ cc 2023-2-19/app.js
@@ -0,0 +1,24 @@
+// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+
+function range(start,end){
+    let all = [];
+    range.push(all)
+    
+    return (range);
+}
+
+// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+
+function sum(array){
+    return array.reduce();
+}
+
+
+
+function range(start,end, step){
+    for(let i = 0; i < range.length; i++ ){
+        let all = range[i];
+    }
+    return (range);
+}
