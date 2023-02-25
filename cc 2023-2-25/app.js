e7939c14462edcdfd710d14f16b4bd3fd9ae3ee9
Watson Pierre <watsonpierrejr@gmail.com>
2023-02-25 00:21:54 -0500

    improved Hello

--- cc 2023-2-25/app.js
+++ cc 2023-2-25/app.js
@@ -2,9 +2,9 @@
 // Complete the following program so that it asks the user for his first and last names, then show the result of the sayHello() function.
 // Say hello to the user
 
-const firstName = prompt("Enter your first Name!")
+const firstName = prompt("Enter your first Name!!")
 
-const lastName = prompt("Enter your last Name!")
+const lastName = prompt("Enter your last Name!!")
 
 function sayHello(firstName, lastName) {
     const message = `Hello, ${firstName} ${lastName}!`;
