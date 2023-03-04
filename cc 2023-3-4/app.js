d64e5e6943dd6ee9c2c484a603c42ff6d946c645
Watson Pierre <watsonpierrejr@gmail.com>
2023-03-04 00:38:52 -0500

    Mimimum of two numbers

---
+++ cc 2023-3-4/app.js
@@ -0,0 +1,10 @@
+// Minimum of two numbers
+// Let's pretend the JavaScript Math.min() function doesn’t exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+
+// // TODO: write the min() function
+
+const min = Math.min;
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
