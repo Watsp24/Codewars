e8d162eb67c231abea06c45c105ee82edfc021d9
Watson Pierre <watsonpierrejr@gmail.com>
2023-04-01 15:10:35 -0400

    number comparison

---
+++ cc 2023-4-1/app.js
@@ -0,0 +1,21 @@
+// Number comparison
+// Write a program that accepts two numbers, then compares their values and displays an appropriate message in all cases.
+
+
+//prep parameters result example pseudocode
+//parameters - two integers
+//return - comparison of numbers
+//example - a:9 is greater than b:5
+
+const numbA = Number(prompt("enter in number a"));
+const numbB = Number(prompt("enter in number a"));
+
+if(numbA === numbB){
+    console.log(`${numbA} is equal to ${numbB}`);
+}else if (numbA < numbB){
+    console.log(`${numbA} is less than ${numbB}`)
+}else if (numbA > numbB){
+    console.log(`${numbA} is greater than ${numbB}`)
+}else{
+    console.log("houston we have a problem")
+}
