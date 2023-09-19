503d20931911085781229eed722f38cf4192ce7c
Watson Pierre <watsonpierrejr@gmail.com>
2023-09-18 20:48:59 -0400

    Pillars

--- cc 2023-9-18/app.js
+++ cc 2023-9-18/app.js
@@ -17,7 +17,7 @@
 
 function pillars(numPill, dist, width) {
     if(numPill > 1) {
-    return (numPill-2)*width + (numPill-1)*dist*100;
+    return (numPill-2)*width + (numPill-1)* dist *100;
     } else {
       return 0;
     }
