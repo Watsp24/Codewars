086665ff044d1430edc0a646fde7a93f535f1494
Watson Pierre <watsonpierrejr@gmail.com>
2023-10-28 11:43:56 -0400

    Terminal Game Turn Function

---
+++ cc 2023-10-28/app.js
@@ -0,0 +1,35 @@
+// DESCRIPTION:
+// Terminal game turn function
+// You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+
+// You are using a library (Game.Logic in C#) that already has the functions below. Create a function named doTurn/DoTurn/do_turn that calls the functions in the proper order as described in the paragraph above.
+
+// - combat
+// - buyHealth
+// - getCoins
+// - printStatus
+// - rollDice
+// - move
+
+
+
+
+//prep 
+//parameter 
+//return function calls in a proper order
+//example roll the dice, move, combat, get coins, buy more health, and print status.
+
+
+function doTurn () {
+    // Call functions here
+    log = ['rollDice','move','combat','getCoins','buyHealth','printStatus'];
+
+
+  }
+  
+//   function doTurn () {
+//     var actions = [rollDice, move, combat, getCoins, buyHealth, printStatus];
+//     actions.forEach(e => e());
+//   }
+  
+  
