af2db203c5edb3ce1ad70e3d33d120ef72310cdb
Watson Pierre <watsonpierrejr@gmail.com>
2023-11-01 10:38:39 -0400

    Neutralisation

---
+++ cc 2023-11-1/app.js
@@ -0,0 +1,46 @@
+// DESCRIPTION:
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+// Worked Example
+// ("+-+", "+--") ➞ "+-0"
+// # Compare the first characters of each string, then the next in turn.
+// # "+" against a "+" returns another "+".
+// # "-" against a "-" returns another "-".
+// # "+" against a "-" returns "0".
+// # Return the string of characters.
+
+// Notes
+// The two strings will be the same length.
+
+
+//Prep 
+//parameters - 2 strings
+//return - the result of the two matching string comparison
+//example - ("+-+", "+--") ➞ "+-0"      ("--++--", "++--++") ➞ "000000"v
+
+
+function neutralise(s1, s2) {
+    // Here be dragons!
+    //loop thru both strings
+    //compare indexes of the string
+    //set up the resulting equation and answer
+    let result = ""
+
+    for(let i = 0; i < s1.length; i++){
+        for (let j = 0;  j < s2.length; i++){
+            if(s1[i] === "+" && s2[i]  === "+"){
+                result += "+";
+            }else if(s1[i] === "-" && s2[i] === "-"){
+                result  += "-";
+            }else{
+                result += "0";
+            }
+        }
+    }
+
+
+    return result;
+  }
