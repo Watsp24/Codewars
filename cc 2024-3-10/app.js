3ac95264b7555026097f54bc25b7462c507cedbe
Watson Pierre <watsonpierrejr@gmail.com>
2024-03-10 17:19:50 -0400

    Days in the year

--- cc 2024-3-10/app.js
+++ cc 2024-3-10/app.js
@@ -38,15 +38,3 @@ function yearDays(year)
   
 
 
-function yearDays(year)
-{ //evenly divisible by 4 = a leap year.
-  if(year % 400 === 0) {
-    return year + ' has 366 days';
-  }
-  else if(year % 4 === 0 && year % 100 !== 0) { //centuries 
-    return year + ' has 366 days';
-  }
-  else {
-    return year + ' has 365 days'; 
-  }
-}
