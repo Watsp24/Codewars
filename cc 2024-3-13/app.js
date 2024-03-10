22eb48d4d2023e3aa80e840130ef0086252e72d9
Watson Pierre <watsonpierrejr@gmail.com>
2024-03-10 17:19:30 -0400

    Days in the year

--- cc 2024-3-13/app.js
+++ cc 2024-3-13/app.js
@@ -1,5 +1,5 @@
-Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
-log
+// Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
+// log
 
 
 
