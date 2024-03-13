1231fb7ddc6e1cdcbfdede37ddbde5a1e0b0614f
Watson Pierre <watsonpierrejr@gmail.com>
2024-03-13 16:26:08 -0400

    Easy Logs

---
+++ cc 2024-3-13/app.js
@@ -0,0 +1,11 @@
+Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
+log
+
+
+
+
+
+function logs(x , a, b){
+    //your code here
+    return Math.log(a * b) / Math.log(x)
+    }
