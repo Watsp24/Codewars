cd50a29482732ea867daa28d9b2e5098b3d43624
Watson Pierre <watsonpierrejr@gmail.com>
2024-03-15 19:01:46 -0400

    Training JS #3: Basic data types--String

--- cc 2024-3-15/app.js
+++ cc 2024-3-15/app.js
@@ -19,7 +19,7 @@ function Dad(){
 }
 function Bee(){
   //select some variable to combine "Bee"
-  return b1+e2+e2;
+  return b1+e2+e2 ;
 }
 function banana(){
   //select some variable to combine "banana"
