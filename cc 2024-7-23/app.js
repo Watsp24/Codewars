21467f3352f94b1b75ac08ba039b7217714ae102
Watson Pierre <watsonpierrejr@gmail.com>
2025-03-31 09:07:40 -0400

    Volume of a Box

---
+++ cc 2024-7-23/app.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Write a simple function that takes as a parameter a date object and returns a boolean value representing whether the date is today or not.
+
+// Make sure that your function does not return a false positive by only checking the day.
+
+
+
+// Parameters Return Example Psuedo Code
+//parameters will be a date objest
+//return boolean true if matches todays date and false if it does not
+//example 
+
+
+
+function isToday(date) {
+    //Code goes here.
+  }
